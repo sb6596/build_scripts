b64e30ec1125dd289714cb7a774b27210097f558
Vojtěch Hejsek <hejsekvojtech@gmail.com>
2017-08-13 23:16:39 +0200

    Added a new device: Huawei Honor 3C (H30-U10)

--- lpad_twrp-5.1.sh
+++ lpad_twrp-5.1.sh
@@ -669,4 +669,27 @@ cd device
 rm -rf $brand
 cd ..
 
-echo "TeamWin Recovery $twrpver has been successfuly built for all LPAD supported devices $branch branch!"
+# Micromax Canvas Hue TWRP build configuration
+
+export device_tree="https://github.com/liquidporting/android_device_huawei_H30-U10.git"
+export brand="huawei"
+export device="H30-U10"
+
+git clone $device_tree -b $branch device/$brand/$device
+. build/envsetup.sh
+lunch omni_$device-eng
+mka recoveryimage > twrp_$device.log
+cd out/target/product/$device
+mv recovery.img twrp-$twrpver-$device.img
+megarm /Root/LPAD/Devices/$device/Recovery/twrp-$twrpver-$device.img
+megarm /Root/LPAD/Devices/$device/Recovery/twrp_$device.log
+megaput --no-progress --path /Root/LPAD/Devices/$device/Recovery twrp-$twrpver-$device.img
+megaput --no-progress --path /Root/LPAD/Devices/$device/Recovery ../../../../twrp_$device.log
+cd ../../../..
+make clean
+rm twrp_$device.log
+cd device
+rm -rf $brand
+cd ..
+
+echo "TeamWin Recovery $twrpver has been successfuly built for all LPAD supported devices using $branch branch!"
