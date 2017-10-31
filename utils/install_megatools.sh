7d43be120dcbb45b71c68d5611a8e1b4a387b4f1
Vojtěch Hejsek <hejsekvojtech@gmail.com>
2017-10-31 18:43:02 +0100

    Fix installation

--- utils/install_megatools.sh
+++ utils/install_megatools.sh
@@ -17,6 +17,8 @@
 #
 
 cd ~
+sudo apt-get update
+sudo apt-get install libtool libglib2.0-dev gobject-introspection libgmp3-dev nettle-dev asciidoc glib-networking openssl libcurl4-openssl-dev libssl-dev
 wget http://megatools.megous.com/builds/megatools-1.9.98.tar.gz
 zcat megatools-1.9.98.tar.gz > megatools-1.9.98.tar
 tar -xf megatools-1.9.98.tar
