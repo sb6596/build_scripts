a22ec6d3a5c871c36591159bea15506c287945ee
Vojtěch Hejsek <hejsekvojtech@gmail.com>
2017-11-11 16:53:56 +0100

    Update lpad_twrp-6.0.sh

--- lpad_twrp-6.0.sh
+++ lpad_twrp-6.0.sh
@@ -23,57 +23,6 @@ export branch="android-6.0"
 # Don't touch this
 VERSION=$( grep "TW_MAIN_VERSION_STR" bootable/recovery/variables.h -m 1 | cut -d \" -f2 )-${TW_DEVICE_VERSION}
 
-# Xiaomi Redmi 4A specific TWRP build configuration
-export BRAND="xiaomi"
-export DEVICE="rolex"
-
-git clone https://github.com/liquidporting/android_device_${BRAND}_${DEVICE}.git -b ${BRANCH} device/${BRAND}/${DEVICE}
-. build/envsetup.sh
-lunch omni_${DEVICE}-eng
-mka recoveryimage > twrp_${DEVICE}.log
-cd out/target/product/${DEVICE}
-if [ -f "recovery.img" ]
-then
-  mv recovery.img twrp-${VERSION}-${DEVICE}.img
-else
-  echo ""
-  echo "*******************************************************************************"
-  echo "Something went wrong during the build process, try checking your device tree."
-  echo "After that, run the script again and see if you messed up something new or not."
-  echo "*******************************************************************************"
-  echo ""
-fi
-
-if [ -f "twrp-${VERSION}-${DEVICE}.img" ]
-then
-  megarm /Root/LPAD/TWRP/twrp-${VERSION}-${DEVICE}.img
-  megarm /Root/LPAD/TWRP/twrp_${DEVICE}.log
-  megaput --no-progress --path /Root/LPAD/TWRP twrp-${VERSION}-${DEVICE}.img
-  megaput --no-progress --path /Root/LPAD/TWRP ../../../../twrp_${DEVICE}.log
-fi
-
-if [ -f "twrp-${VERSION}-${DEVICE}.img" ]
-then
-  cd ../../../..
-  rm twrp_${DEVICE}.log
-  make clean
-  cd device
-  rm -rf ${BRAND}
-  cd ..
-else
-  rm twrp_${DEVICE}.log
-  make clean
-  cd device
-  rm -rf ${BRAND}
-  cd ..
-  echo ""
-  echo "**************************************************************"
-  echo "The build process of TWRP Recovery failed for device ${DEVICE}"
-  echo "**************************************************************"
-  echo ""
-  exit
-fi
-
 # Xiaomi Redmi 4 (China) specific TWRP build configuration
 export BRAND="xiaomi"
 export DEVICE="prada"
