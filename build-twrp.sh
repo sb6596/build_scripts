f17c33b47bf576a4c00dd77accb33341ab37e368
ShubhamB <shubhamprince111@gmail.com>
2017-12-11 18:04:32 +0530

    Update for Custom build

--- build-twrp.sh
+++ build-twrp.sh
@@ -22,7 +22,7 @@ LOG="$2"
 CLEAN="$3"
 
 export TW_DEVICE_VERSION="0"
-export VERSION=$( grep "TW_MAIN_VERSION_STR" bootable/recovery/variables.h -m 1 | cut -d \" -f2 )-${TW_DEVICE_VERSION}
+export VERSION=$( grep "TW_VERSION_STR" bootable/recovery/variables.h -m 1 | cut -d \" -f2 )-${TW_DEVICE_VERSION}
 export OUT_LOG="twrp-${VERSION}-${DEVICE}.log"
 export OUT_PATH="out/target/product/${DEVICE}"
 export OUT_RECOVERY_IMAGE="twrp-${VERSION}-${DEVICE}.img"
